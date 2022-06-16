1f062bb954cc7f16bb1e6196ec55045b4f82bae3
Roland Guijt <roland.guijt@gmail.com>
2022-06-16 10:45:47 +0200

    Revert "Revert "Add console client""
    This reverts commit 0bbcfd9b68bc789c2485be2120b72ee1b86567a0.

---
+++ ConsoleClient/Auction.cs
@@ -0,0 +1,8 @@
+﻿
+    public class Auction
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; } = "";
+        public int CurrentBid { get; set; }
+    }
+
---
+++ ConsoleClient/AuctionNotify.cs
@@ -0,0 +1,7 @@
+﻿
+    public class AuctionNotify
+    {
+        public int AuctionId { get; set; }
+        public int NewBid { get; set; }       
+    }
+
